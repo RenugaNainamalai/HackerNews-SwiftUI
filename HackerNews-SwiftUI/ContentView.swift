df23b2dee9c660c1af9313f003361b969065bcda
Vidyutha <Vidhyu22@icloud.com>
2024-07-12 15:54:16 +0530

    Initial Commit

---
+++ HackerNews-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Vidyu on 12/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerNews-SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Vidyu on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
